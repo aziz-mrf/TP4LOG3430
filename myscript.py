cc6ab2f312a81f90e150d6a55ce66d44bde7119e
aziz-mrf <azizmaaref@gmail,com>
2025-11-05 14:24:44 -0500

    Added automatic bisect in pipeline 2

--- myscript.py
+++ myscript.py
@@ -1,8 +1,8 @@
 import os
 
 # Define your commits here (bad = tests fail, good = tests pass)
-BAD_COMMIT = "1d8748281263e8e7efe7b85c828cd3f600d96bfc"
-GOOD_COMMIT = "a9f20e8a511665d81967342e7829e737395944d3"
+BAD_COMMIT = "c1a4be04b972b6c17db242fc37752ad517c29402"
+GOOD_COMMIT = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 
 os.system(f"git bisect start {BAD_COMMIT} {GOOD_COMMIT}")
 os.system("git bisect run python manage.py test")
